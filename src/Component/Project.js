43f6c4b813478ff1e46eb6efa86d1171904373c3
DHATCHAYINI-2003 <dhatchayini192003@gmail.com>
2023-04-05 15:15:48 +0530

    First Commit for GITHUB

---
+++ src/Component/Project.js
@@ -0,0 +1,26 @@
+import React from 'react'
+export default function Project() {
+  return (
+    <div id='pro'>
+    <h1 id='head'>Non-Veg</h1>
+    <h2 id='deg'>Fish And Mutton Briyani</h2>
+    <img id='logo' alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT91arOIdbJE-Ws7lzgNewUHq_frEANi3oJZA&usqp=CAU'></img>
+    <marquee>Any south Indian non-vegetarian meal is incomplete without a Masala Fish Fry.It is a very common and staple dish that is prepared throughout the coastal region of India</marquee>
+    <img id='logo2' alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT66d8jsPBIoaw0xeBZWHL_T9OQPOBJDKeAlg&usqp=CAU'></img>
+    <marquee>Biryani is one of the most popular dishes in South Asia, as well as among the diaspora from the region. Similar dishes are also prepared in other parts of the world such as in Iraq, Myanmar, Thailand, and Malaysia</marquee>
+    <div id='list'>
+    <h2>Fish Details:</h2>
+    <h3><ul><a class='pro' href='https://en.wikipedia.org/wiki/Fish_fry'>About</a></ul></h3>
+    <h3><ul><a class='pro' href='https://www.vahrehvah.com/indianfood/best-indian-fish-fry-recipes'>Recipe</a></ul></h3>
+    </div>
+    <div id='list2'>
+    <h2>Mutton Briyani Details:</h2>
+    <h3><ul><a class='pro' href='https://en.wikipedia.org/wiki/Biryani'>About</a></ul></h3>
+    <h3><ul><a class='pro' href='https://www.indianhealthyrecipes.com/mutton-biryani/'>Recipe</a></ul></h3>
+    </div>
+    <h2>Order In Here : </h2>
+    <h3><a href='https://www.swiggy.com/restaurants/fish-and-fry-kavundampalayam-coimbatore-67639'>Fish</a></h3>
+    <h3><a href='https://www.swiggy.com/restaurants/star-mutton-dum-biryani-mangalagiri-vijayawada-236578'>Mutton Biryani</a></h3>
+    </div>
+  )
+}
